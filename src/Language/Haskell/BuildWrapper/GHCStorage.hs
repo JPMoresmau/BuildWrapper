8ba6ffafa18cfc95a68304126b7fb80a969de284
JP Moresmau <jp@moresmau.fr>
2012-02-06 22:30:48 +0100

    doc

--- src/Language/Haskell/BuildWrapper/GHCStorage.hs
+++ src/Language/Haskell/BuildWrapper/GHCStorage.hs
@@ -1,4 +1,16 @@
 {-# LANGUAGE CPP,OverloadedStrings,PatternGuards #-}
+-- |
+-- Module      : Language.Haskell.BuildWrapper.GHCStorage
+-- Author      : JP Moresmau
+-- Copyright   : (c) JP Moresmau 2012
+-- License     : BSD3
+-- 
+-- Maintainer  : jpmoresmau@gmail.com
+-- Stability   : beta
+-- Portability : portable
+-- 
+-- Store to disk in JSON format the results of the GHC AST build, and the build flags
+-- this helps us with performance (we only call GHC when the file has changed, not everytime we want to find what's at a given source point
 module Language.Haskell.BuildWrapper.GHCStorage where
 
 import Language.Haskell.BuildWrapper.Base
@@ -7,7 +19,6 @@ import Data.Generics
 import System.Directory
 import System.FilePath
 
--- import qualified GHC.Paths
 import PprTyThing
 import GHC
 import Outputable
@@ -40,28 +51,43 @@ import Data.Attoparsec.Number (Number(I))
 import System.Time (ClockTime)
 
 
-getInfoFile :: FilePath -> FilePath
+-- | get the file storing the information for the given source file
+getInfoFile :: FilePath -- ^ the source file
+        -> FilePath
 getInfoFile fp= let 
         (dir,file)=splitFileName fp
-        in combine dir ("." ++ (addExtension file ".bwinfo"))
+        in combine dir ('.' : addExtension file ".bwinfo")
 
-clearInfo :: FilePath -> IO()
+-- | remove the storage file
+clearInfo :: FilePath -- ^ the source file
+        -> IO()
 clearInfo fp =do
         let ghcInfoFile=getInfoFile fp
         removeFile ghcInfoFile
 
-storeBuildFlagsInfo :: FilePath -> (BuildFlags,[BWNote]) -> IO()
+-- | store the build flags
+storeBuildFlagsInfo :: FilePath -- ^ the source file
+        -> (BuildFlags,[BWNote]) -- ^  build flags and notes
+        -> IO()
 storeBuildFlagsInfo fp bf=setStoredInfo fp "BuildFlags"  (toJSON bf)
 
-storeGHCInfo :: FilePath -> TypecheckedSource -> IO()
+-- | store the GHC generated AST
+storeGHCInfo :: FilePath -- ^ the source file
+        -> TypecheckedSource -- ^ the GHC AST
+        -> IO()
 storeGHCInfo fp tcs=setStoredInfo fp "AST"  (dataToJSON tcs)
 
-readGHCInfo :: FilePath -> IO(Maybe Value)
+-- | read the GHC AST as a JSON value
+readGHCInfo :: FilePath -- ^ the source file
+        -> IO(Maybe Value)
 readGHCInfo fp=do
        (Object hm)<-readStoredInfo fp
        return $ HM.lookup "AST" hm
 
-readBuildFlagsInfo :: FilePath -> ClockTime -> IO (Maybe (BuildFlags,[BWNote]))
+-- | read the build flags and notes as a JSON value
+readBuildFlagsInfo :: FilePath -- ^ the source file
+        -> ClockTime -- ^ time the cabal file was changed. If the file was changed after the storage file, we return Nothing
+        -> IO (Maybe (BuildFlags,[BWNote]))
 readBuildFlagsInfo fp ct=do
        let ghcInfoFile=getInfoFile fp
        ex<-doesFileExist ghcInfoFile
@@ -76,14 +102,20 @@ readBuildFlagsInfo fp ct=do
                    else return Nothing
              else return Nothing
 
-setStoredInfo :: FilePath -> T.Text -> Value -> IO()
+-- | utility function to store the given value under the given key 
+setStoredInfo :: FilePath -- ^ the source file
+        -> T.Text -- ^ the key under which the value will be put 
+        -> Value -- ^ the value
+        -> IO()
 setStoredInfo fp k v=do
         let ghcInfoFile=getInfoFile fp
         (Object hm)<-readStoredInfo fp
         let hm2=HM.insert k v hm
         BSS.writeFile ghcInfoFile $ BSS.concat $ BS.toChunks $ encode $ Object hm2
 
-readStoredInfo :: FilePath -> IO Value
+-- | read the top JSON value containing all the information
+readStoredInfo :: FilePath  -- ^ the source file
+        -> IO Value
 readStoredInfo fp=do
        let ghcInfoFile=getInfoFile fp
        ex<-doesFileExist ghcInfoFile
@@ -94,6 +126,7 @@ readStoredInfo fp=do
                 else return Nothing
        return $ fromMaybe (object []) mv
 
+-- | convert a Data into a JSON value, with specific treatment for interesting GHC AST objects, and avoiding the holes
 dataToJSON :: Data a =>a -> Value
 dataToJSON  = 
   generic `ext1Q` list `extQ` string `extQ` fastString `extQ` srcSpan 
@@ -105,20 +138,20 @@ dataToJSON  =
                 -- object [(T.pack $ showConstr (toConstr t)) .= sub ] 
         string     = Data.Aeson.String . T.pack :: String -> Value
         fastString:: FastString -> Value
-        fastString fs= object ["FastString" .= (T.pack $ show fs)] 
+        fastString fs= object ["FastString" .= T.pack (show fs)] 
         list l     = arr $ map dataToJSON l
         arr a = let
-                sub=filter (/= Null) $ a
+                sub=filter (/= Null) a
                 in case sub of
                      [] -> Null
                      [x] -> x
                      _ -> toJSON sub          
         name :: Name -> Value
-        name  n     = object ((nameAndModule n) ++["GType" .=  (string $ "Name"),"HType".= (string $ (if isValOcc (nameOccName n) then "v" else "t"))])
+        name  n     = object (nameAndModule n ++["GType" .= string "Name","HType".= string (if isValOcc (nameOccName n) then "v" else "t")])
         occName :: OccName -> Value
-        occName o   = object ["OccName" .=  (string $ OccName.occNameString o),"HType".= (string $ (if isValOcc o then "v" else "t"))]
+        occName o   = object ["OccName" .= string (OccName.occNameString o),"HType" .= string (if isValOcc o then "v" else "t")]
         modName  :: ModuleName -> Value
-        modName m= object [ "Name" .=  (string $ showSDoc $ ppr m),"GType" .= (string "ModuleName"),"HType".= (string $ "m")]
+        modName m= object [ "Name" .= string (showSDoc $ ppr m),"GType" .= string "ModuleName","HType" .= string "m"]
         srcSpan :: SrcSpan -> Value
         srcSpan src 
                 | isGoodSrcSpan src   = object[ "SrcSpan" .= toJSON [srcLoc $ srcSpanStart src, srcLoc $ srcSpanEnd src]] 
@@ -136,7 +169,7 @@ dataToJSON  =
         var :: Var -> Value
         var  v     = typedVar v (varType v)
         dataCon ::  DataCon -> Value
-        dataCon  d  = object ((nameAndModule $ dataConName d) ++["GType" .=  (string $ "DataCon")])
+        dataCon  d  = object (nameAndModule (dataConName d) ++ ["GType" .= string "DataCon"])
 --        simple:: T.Text -> String -> Value
 --        simple nm v=object [nm .= T.pack v]
         simpleV:: T.Text -> Value -> Value
@@ -154,11 +187,11 @@ dataToJSON  =
                         Just (t,v)-> typedVar v t
                         Nothing->generic ev
         typedVar :: Var -> Type -> Value
-        typedVar v t=object ((nameAndModule $ varName v)++ [
-                                "GType" .= (string "Var")
-                                ,"Type" .= (string $ showSDocUnqual $ pprTypeForUser True $ t)
-                                ,"QType" .= (string $ showSDoc $ pprTypeForUser True $ t)
-                                ,"HType".= (string $ (if isValOcc (nameOccName (Var.varName v)) then "v" else "t"))])
+        typedVar v t=object (nameAndModule (varName v) ++
+                ["GType" .= string "Var",
+                "Type" .= string (showSDocUnqual $ pprTypeForUser True t),
+                "QType" .= string (showSDoc $ pprTypeForUser True t),
+                "HType" .=  string (if isValOcc (nameOccName (Var.varName v)) then "v" else "t")])
 
         nameSet = const $ Data.Aeson.String "{!NameSet placeholder here!}" :: NameSet -> Value
                 
@@ -170,11 +203,13 @@ dataToJSON  =
         nameAndModule n=let
                 mn=maybe "" (showSDoc . ppr . moduleName) $ nameModule_maybe n
                 na=showSDocUnqual $ ppr n
-                in ["Module" .= (string mn), "Name" .= (string na)]
+                in ["Module" .= string mn, "Name" .= string na]
 
+-- | debug function: shows on standard output the JSON representation of the given data
 debugToJSON :: Data a =>a -> IO()
 debugToJSON = BS.putStrLn . encode . dataToJSON
 
+-- | debug searching thing at point in given data
 debugFindInJSON :: Data a => Int -> Int -> a -> IO()
 debugFindInJSON l c a= do
         let v=dataToJSON a
@@ -185,56 +220,67 @@ debugFindInJSON l c a= do
                         BS.putStrLn $ encode rv
                 Nothing->putStrLn "nothing found!"
 
-type FindFunc=(Value -> Bool)
+-- | simple type for search function
+type FindFunc= Value -> Bool
 
-
-findInJSONFormatted :: Bool -> Bool -> Maybe Value -> String
+-- | find in JSON AST and return the string result
+findInJSONFormatted :: Bool -- ^ should the output be qualified?
+        -> Bool -- ^ should the output be fully typed?
+        -> Maybe Value -- ^ result of search 
+        -> String
 findInJSONFormatted qual typed (Just (Object m)) | Just (String name)<-HM.lookup "Name" m=let
         tn=T.unpack name
         qn=if qual
                 then 
-                     let mo=maybe "" (\(String s)->(T.unpack s)++".") $ HM.lookup "Module" m
+                     let mo=maybe "" (\(String s)->T.unpack s ++ ".") $ HM.lookup "Module" m
                      in mo ++ tn
                 else tn
         in if typed then
                         let mt=HM.lookup (if qual then "QType" else "Type") m
                         in case mt of
-                                Just (String t)->qn ++ " :: " ++ (T.unpack t)
+                                Just (String t)->qn ++ " :: " ++ T.unpack t
                                 _ -> tn
                 else
                        let mt=HM.lookup "HType" m
                        in case mt of
-                                Just (String t)->qn ++ " " ++ (T.unpack t)
+                                Just (String t)->qn ++ " " ++ T.unpack t
                                 _ -> tn
 findInJSONFormatted _ _ _="no info"
 
-findInJSON :: FindFunc -> Value -> Maybe Value
+-- | find in JSON AST
+findInJSON :: FindFunc -- ^ the evaluation function  
+        -> Value -- ^ the root object containing the AST 
+        -> Maybe Value
 findInJSON f (Array arr) | not $ V.null arr=let
         v1=arr V.! 0
-        in if f v1 && V.length arr==2
+        in if f v1 && V.length arr==2 -- we have an array of two elements, the first one being a matching SrcSpan we go down the second element
            then 
                 let
                         mv=findInJSON f $ arr V.! 1
                 in case mv of
-                        Just rv-> Just rv
-                        Nothing -> Just $ arr V.! 1
+                        Just rv-> Just rv -- found something underneath
+                        Nothing -> Just $ arr V.! 1 -- found nothing underneath, return second element of the array
            else
-                let rvs=catMaybes $ V.toList $ fmap (findInJSON f) arr
+                let rvs=catMaybes $ V.toList $ fmap (findInJSON f) arr -- other case of arrays: check on each element
                 in case rvs of
-                        (x:_)->Just x
+                        (x:_)->Just x -- return first match
                         []->Nothing
-findInJSON f (Object obj)=let rvs=mapMaybe (findInJSON f) $ HM.elems obj
+findInJSON f (Object obj)=let rvs=mapMaybe (findInJSON f) $ HM.elems obj -- in a complex object: check on contained elements
                 in case rvs of
                         (x:_)->Just x
                         []->Nothing
 findInJSON _ _= Nothing
 
-overlap :: Int -> Int -> Value -> Bool
+-- | overlap function: find whatever is at the given line and column
+overlap :: Int  -- ^ line
+        -> Int -- ^ column
+        -> FindFunc
 overlap l c (Object m) | 
         Just pos<-HM.lookup "SrcSpan" m,
         Just (l1,c1,l2,c2)<-extractSourceSpan pos=l1<=l && c1<=c && l2>=l && c2>=c
 overlap _ _ _=False
 
+-- | extract the source span from JSON
 extractSourceSpan :: Value -> Maybe (Int,Int,Int,Int)
 extractSourceSpan (Array arr) | V.length arr==2 = do
         let v1=arr V.! 0
@@ -244,13 +290,14 @@ extractSourceSpan (Array arr) | V.length arr==2 = do
         return (l1,c1,l2,c2)
 extractSourceSpan _ =Nothing
 
+-- | extract the source location from JSON
 extractSourceLoc :: Value -> Maybe (Int,Int)
 extractSourceLoc (Object m) | 
         Just (Number(I l))<-HM.lookup "line" m,
         Just (Number(I c))<-HM.lookup "column" m=Just (fromIntegral l,fromIntegral c)  
 extractSourceLoc _ = Nothing       
 
-
+-- | resolve the type of an expression
 typeOfExpr :: HsExpr Var -> Maybe (Type,Var)
 typeOfExpr (HsWrap wr (HsVar ident)) =
   let -- Unwrap a HsWrapper and its associated type
@@ -272,7 +319,7 @@ typeOfExpr (HsWrap wr (HsVar ident)) =
 #ifdef WPINLINE
       unwrap WpInline t          = t
 #endif
-  in  Just $ (reduce_type $ unwrap wr (varType ident),ident)
+  in  Just (reduceType $ unwrap wr (varType ident), ident)
 -- All other search results produce no type information
 typeOfExpr _ = Nothing
 
@@ -283,13 +330,13 @@ typeOfExpr _ = Nothing
 -- @
 -- where @[t'/v]@ is the substitution of @t'@ for @v@.
 --
-reduce_type :: Type -> Type
-reduce_type (AppTy (ForAllTy tv b) t) =
-    reduce_type (subst_type tv t b)
-reduce_type t = t
+reduceType :: Type -> Type
+reduceType (AppTy (ForAllTy tv b) t) =
+    reduceType (substType tv t b)
+reduceType t = t
 
-subst_type :: TyVar -> Type -> Type -> Type
-subst_type v t' t0 = go t0
+substType :: TyVar -> Type -> Type -> Type
+substType v t' t0 = go t0
   where
     go t = case t of
       TyVarTy tv 
@@ -308,14 +355,4 @@ subst_type v t' t0 = go t0
     go_pt (IParam i t)   = IParam i (go t)
     go_pt (EqPred t1 t2) = EqPred (go t1) (go t2)
     
-    
---haddockType  :: SearchResult a -> String
---haddockType (FoundName n)
---        | isValOcc (nameOccName n)="v"
---        | otherwise= "t"
---haddockType (FoundId i)
---        | isValOcc (nameOccName (Var.varName i))="v"
---        | otherwise= "t"
---haddockType (FoundModule _)="m"
---haddockType _="t"    
     
