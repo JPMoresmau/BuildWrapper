9928b3f00b24dd9e09f328724958794c7e327ff5
jpmoresmau <jp@moresmau.fr>
2014-11-16 18:45:02 +0100

    Fix for haddock

--- src/Language/Haskell/BuildWrapper/GHC.hs
+++ src/Language/Haskell/BuildWrapper/GHC.hs
@@ -959,7 +959,7 @@ preprocessSource contents literate=
                         | ('#':_)<-l =addPPToken "PP" (l,c) (ts2,l2,lineBehavior l f) 
                         | Just (l',s,e,f2)<-pragmaExtract l f=
                           (TokenDef "P" (mkFileSpan c s c e) : ts2 ,l':l2,f2)
-                        -- | "{-# " `List.isPrefixOf` l=addPPToken "P" (l,c) (ts2,"":l2,pragmaBehavior l f) 
+                        -- "{-# " `List.isPrefixOf` l=addPPToken "P" (l,c) (ts2,"":l2,pragmaBehavior l f) 
                         | (Indent n)<-f=(ts2,l:(replicate n (takeWhile (== ' ') l) ++ l2),Start)
                         | otherwise =(ts2,l:l2,Start)
                 ppSLit :: ([TokenDef],[String],PPBehavior) -> (String,Int) -> ([TokenDef],[String],PPBehavior)
